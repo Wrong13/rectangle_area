9fb65d0ac4dd29bf32e76e127e3cad763d7e9dae
Wrong <ilak71363@gmail.com>
2023-02-24 18:36:04 +0600

    Finnaly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("{:?}",rect);
+    println!("Площадь {}",area(&rect));
+}
+
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
+}
+
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32,
 }
