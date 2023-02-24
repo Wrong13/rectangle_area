6ec584ab63087a6ddb9a6ef67846b49d5967d5e5
Wrong <ilak71363@gmail.com>
2023-02-24 18:55:07 +0600

    Проверка влезет ли один прямоугольник в другой

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,20 @@ fn main() {
     let rect = Rectangle {
         width: 30,
         height: 50,
+        name : String::from("прямоуго1ник"),
     };
+    let rect2 = Rectangle {
+        width: 20,
+        height: 40,
+        name : String::from("прямо2гольник"),
+    };
+
+    if rect.can_hold(&rect2) == true {println!("В {} влезет {}",
+        rect.name,rect2.name);}
+    else {
+        println!("В {} не влезет {}", rect.name,rect2.name);
+    }
+
     println!("{:?}",rect);
     println!("Площадь {}",rect.area());
 }
@@ -11,10 +24,15 @@ fn main() {
 struct Rectangle {  // Описание
     width: u32,
     height: u32,
+    name : String,
 }
 
 impl Rectangle {    // Реализация
     fn area(&self) -> u32 {     // Метод
         self.width * self.height    // self-псевдоним типа для которого реализован блок
     }   // Тобишь сам себя 
+
+    fn can_hold(&self,other: &Rectangle) -> bool {      // Проверка вместится ли иной прямоугольник в текущий
+        self.width > other.width && self.height > other.height
+    }
 }
